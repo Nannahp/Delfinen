fd067df13e8fe5b6f1dacba6cecaaf5bf0e63033
Nanna Petersen <nannahofgaard@hotmail.com>
2023-12-04 10:11:55 +0100

    colors

---
+++ src/ConsoleColor.java
@@ -0,0 +1,25 @@
+enum ConsoleColor {
+    //Color end string, color reset
+    RESET("\033[0m"),
+
+    // Regular Colors. Normal color, no bold, background color etc.
+    BLACK("\033[0;30m"),    // BLACK
+    RED("\033[0;31m"),      // RED
+    GREEN("\033[0;32m"),    // GREEN
+    YELLOW("\033[0;33m"),   // YELLOW
+    BLUE("\033[0;34m"),     // BLUE
+    MAGENTA("\033[0;35m"),  // MAGENTA
+    CYAN("\033[0;36m");    // CYAN
+
+
+    private final String code;
+
+    ConsoleColor(String code) {
+        this.code = code;
+    }
+
+    @Override
+    public String toString() {
+        return code;
+    }
+}
